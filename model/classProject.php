6ccd92e83378cd1f1b7b6ad47b9ce9766410c877
Alderjanio <alderjanio@hotmail.com>
2021-01-11 23:46:29 -0400

    Criação do método Add (cadastro de projetos no banco de dados)

--- model/classProject.php
+++ model/classProject.php
@@ -7,6 +7,19 @@ class ClassProject extends ClassConnection{
     public function addProjects($nameProject,$dataInicio,$DataTermino,$valorProject,$nameRisco,$nameParticipantes)
     {
         var_dump($nameProject,$dataInicio,$DataTermino,$valorProject,$nameRisco,$nameParticipantes);
+       
+        $sql = "INSERT INTO tbl_project VALUES (null,?,?,?,?,?,?)";
+        $add = $this->connectDB()->prepare($sql);
+        $add->bindValue(1, $nameProject);
+        $add->bindValue(2, $dataInicio);
+        $add->bindValue(3, $DataTermino);
+        $add->bindValue(4, $valorProject);
+        $add->bindValue(5, $nameRisco);
+        $add->bindValue(6, $nameParticipantes);
+    
+        $add->execute();
+  
+    echo json_encode('ok');
     }
 
 }
